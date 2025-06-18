b6ffcb1789b98f414dcf8d0749bc4fea7fbff4bd
서교필 <gyofeel@sk.com>
2025-06-18 17:50:53 +0900

    build: tailwind, shadcn/ui 초기 환경 설정
    - conflict resolving

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,10 +1,10 @@
 module.exports = {
-    extends: [
-      'plugin:@typescript-eslint/recommended',
-      'plugin:prettier/recommended'
-    ],
-    plugins: ['@typescript-eslint', 'prettier'],
-    rules: {
-      'prettier/prettier': 'error'
-    }
-  }
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  plugins: ['@typescript-eslint', 'prettier'],
+  rules: {
+    'prettier/prettier': 'error',
+  },
+};
