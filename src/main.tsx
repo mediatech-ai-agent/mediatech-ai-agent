957b9e27ebdef22a2616fccaf65e4f9d37f0125e
서교필 <gyofeel@sk.com>
2025-07-02 19:38:16 +0900

    build: react-query 개발자 도구 추가

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import './index.css';
 import App from './App.tsx';
 
@@ -14,5 +15,6 @@ createRoot(document.getElementById('root')!).render(
         <App />
       </BrowserRouter>
     </QueryClientProvider>
+    <ReactQueryDevtools />
   </StrictMode>
 );
