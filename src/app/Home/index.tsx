0243252babf4a54aad67e6503fb5cd1bd977f707
Gyopil Seo <gyofeel318@gmail.com>
2025-07-23 09:46:43 +0900

    chore: reverse commit

--- src/app/Home/index.tsx
+++ src/app/Home/index.tsx
@@ -1,15 +1,5 @@
-import { BgMenuFull } from '../../components/BgMenuFull';
-
 const Home = () => {
-  return (
-    <div className="flex min-h-screen">
-      {/* 좌측 메뉴 */}
-      <div className="fixed top-10 z-10 left-15">
-        <BgMenuFull width={280} height={1000}>
-        </BgMenuFull>
-      </div>
-    </div>
-  );
+  return <div>Home</div>;
 };
 
 export default Home;
--- src/components/index.ts
+++
@@ -1,5 +0,0 @@
-export {
-  BgMenuFull,
-  BgMenuFullResponsive,
-  BgMenuFullMobile,
-} from './BgMenuFull';
